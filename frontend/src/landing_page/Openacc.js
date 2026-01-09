7937588a8c1a751dab992f385f81d48322b9dd61
YAKSHMEHTA <yakshvardhansinghmehta@gamil.com>
2026-01-09 21:01:52 +0530

    .

---
+++ frontend/src/landing_page/Openacc.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Openacc() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Openacc
--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Hero from './Hero'
+import Awards from './Awards'
+import Stats from './Stats'
+import Pricing from './Pricing'
+import Education from './Education'
+import Openacc from '../Openacc'
+import Navbar from '../Navbar'
+import Footer from '../Footer'
 
 function HomePage() {
   return (
     <div>
-      
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Awards></Awards>
+      <Stats></Stats>
+      <Pricing></Pricing>
+      <Education></Education>
+      <Openacc></Openacc>
+      <Footer></Footer>
     </div>
   )
 }
