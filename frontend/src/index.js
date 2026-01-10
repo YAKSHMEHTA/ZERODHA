51dbc56257a4cf3f03fc5e947f718f9ac0e60a19
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-01-10 12:16:37 +0530

    Fix git email for contributions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 import HomePage from './landing_page/home/HomePage';
 
 
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
 function Hero() {
   return (
     <div>
-      
+      <h1>Hero <i class="fa-brands fa-amazon"></i></h1>
+
     </div>
   )
 }
