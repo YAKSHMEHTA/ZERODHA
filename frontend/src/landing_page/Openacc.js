59a863de92c1e301fd77d695eb992d98f83ce286
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-01-11 16:20:36 +0530

    Navbar

--- frontend/src/landing_page/Openacc.js
+++ frontend/src/landing_page/Openacc.js
@@ -3,7 +3,11 @@ import React from 'react'
 function Openacc() {
   return (
     <div>
-      
+      <div className='row text-center '>
+        <h1 className='mt-5 mb-3'>Invest in everything</h1>
+        <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+        <button className='p-3 btn btn-primary fs-5 mt-2' style={{width:"30%",margin:"0 auto",}}>Sign up for free</button>
+      </div>
     </div>
   )
 }
