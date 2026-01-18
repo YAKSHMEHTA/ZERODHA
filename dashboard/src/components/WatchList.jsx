966fadab35207380a283d8400d15684d6ca1940e
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-01-18 09:56:24 +0530

    watchlist

--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import {watchlist} from "../data/Data";
+import { Tooltip, Grow } from "@mui/material";
 
 const WatchList = () => {
   return (
@@ -11,12 +13,35 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length}/ 50</span>
       </div>
-
+      {watchlist.map((stock, index) => {
+        <WatchListItem stock={stock} key={index} />
+      })}
       <ul className="list"></ul>
     </div>
   );
 };
 
 export default WatchList;
+
+const WatchListItem = ({ stock, index }) => {
+  const [show, setShow] = useState(false);
+  const handelMouseEnter = (e) => {
+    setShow(true);
+  };
+  const handelMouseleave = (e) => {
+    setShow(false);
+  };
+
+  return (
+    <li onMouseEnter={handelMouseEnter} onMouseLeave={handelMouseleave}>
+      <div className="item">
+        <p className={stock.isDown ? "down":"up"}>{stock.name}</p>
+        <div className="itemInfo">
+          
+        </div>
+      </div>
+    </li>
+  );
+};
