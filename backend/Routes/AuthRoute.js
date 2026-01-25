e343731a8a68332cd09f92cd0b309e4dca708dfd
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-01-25 14:54:54 +0530

    midlleware

--- backend/Routes/AuthRoute.js
+++ backend/Routes/AuthRoute.js
@@ -1,7 +1,10 @@
-const { Signup } = require("../Controllers/AuthController");
+const { Signup,Login } = require("../Controllers/AuthController");
+const {userVerification} = require("../Middlewares/AuthMiddleware")
 const router = require("express").Router();
 
 router.post("/signup", Signup);
+router.post('/',userVerification)
+router.post('/login', Login)
 
 
 module.exports = router;
