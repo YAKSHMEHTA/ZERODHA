622ed8fd85c6aa63eec0a8d8a1bee5ee54d079b4
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-01-27 20:04:54 +0530

    general context

--- dashboard/src/components/GeneralContext.jsx
+++ dashboard/src/components/GeneralContext.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState }  from 'react'
+import BuyActionWindow from './BuyActionWindow'
 
-function GeneralContext() {
+
+const GeneralContext = React.createContext({
+  openBuyWindow :(uid) => {},
+  closeBuyWindow:()=>{}
+})
+
+export const GeneralContextProvider = (props)=> {
+
+  const [isBuyWindowOpen,setIsBuyWindowOpen] = useState(false)
+  const [selectedStockUid,setSelectedStockUid] = useState("")
+  
+  const handelOpen =(uid)=>{
+    setIsBuyWindowOpen(true)
+    setSelectedStockUid(uid)
+  }
+
+  const handelClose = ()=>{
+    setSelectedStockUid("")
+    setIsBuyWindowOpen(true)
+  }
+
   return (
-    <div>
-      
-    </div>
+    <GeneralContext.Provider
+    value={{
+      openBuyWindow:handelOpen,
+      closeBuyWindow:handelClose,
+    }}
+    >
+      {props.children}
+      {isBuyWindowOpen && <BuyActionWindow uid={selectedStockUid} />}
+    </GeneralContext.Provider>
   )
 }
 
