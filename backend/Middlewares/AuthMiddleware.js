0bc7314ea18d092d5eae5b241e0b2ddc608665c2
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-02-10 13:26:22 +0530

    token

--- backend/Middlewares/AuthMiddleware.js
+++ backend/Middlewares/AuthMiddleware.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 module.exports.userVerification = (req, res) => {
   const token = req.cookies.token
   if (!token) {
+    console.log('Token Not FOund so failed');
     return res.json({ status: false })
   }
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
