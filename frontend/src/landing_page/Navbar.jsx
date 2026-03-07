8dbb47a741679c256bcc9e23f9d20293558d95b1
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-03-07 09:39:55 +0530

    navbar

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -5,14 +5,14 @@ function Navbar() {
   return (
     <nav className="nav-parent w-full p-3" style={{ backgroundColor: "#fff" }}>
       <div className="container p-2 flex ">
-        <Link className="navbar-brand bg-red-600 " to="/login">
+        <Link className="navbar-brand" to="/login">
           <img src="/media/logo.svg" style={{ width: "25%",height:"" }} alt="yaksh" />
         </Link>
         <div className="navright">
-          <Link className="" to="/product">Product</Link>
-          <Link to="pricing">Pricing</Link>
-          <Link to="support">Support</Link>
-          <Link to="about">About</Link>
+          <Link className="nav-link" to="/product">Product</Link>
+          <Link className="nav-link" to="pricing">Pricing</Link>
+          <Link className="nav-link" to="support">Support</Link>
+          <Link className="nav-link" to="about">About</Link>
         </div>
       </div>
     </nav>
